9aa70f875451458433b46f7b836c627c3c81aa61
Pajanec <mario.pijanec@gmail.com>
2020-04-10 17:28:13 +0000

    Finished contact form page, added emailjs on it

---
+++ assets/js/emailsend.js
@@ -0,0 +1,19 @@
+function sendMail(contactForm) {
+    emailjs.send("default_service", "travel", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "packet_information": contactForm.packetinformation.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  
+}
+
+
+
