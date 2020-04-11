4d1147a58faf0d2bb387b3c86f36aa19292ff022
Pajanec <mario.pijanec@gmail.com>
2020-04-11 16:44:27 +0000

    Added working map with  markers and zoom in/center functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,44 @@
-  function initAutocomplete() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -33.8688, lng: 151.2195},
-          zoom: 13,
-          mapTypeId: 'roadmap'
-        });
+function initMap() {
+ 
+  var europe = {lat: 48.270611, lng:9.243381};
+  var lucerne ={lat: 47.050321, lng:8.307818};
+  var granada ={lat: 37.173484, lng:-3.598352};
+  var arles ={lat: 43.676355, lng:4.624997};
+  
+  var map = new google.maps.Map(
+      document.getElementById('map'), {
+          zoom: 4, 
+          center: europe,
+        disableDefaultUI: true
+    });
+  
 
-        // Create the search box and link it to the UI element.
-        var input = document.getElementById('pac-input');
-        var searchBox = new google.maps.places.SearchBox(input);
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  var marker = new google.maps.Marker({position: lucerne, map: map  });
+  var spain= new google.maps.Marker({position: granada, map: map  });
+  var france = new google.maps.Marker({position: arles, map: map });
 
-        // Bias the SearchBox results towards current map's viewport.
-        map.addListener('bounds_changed', function() {
-          searchBox.setBounds(map.getBounds());
-        });
 
-        var markers = [];
-        // Listen for the event fired when the user selects a prediction and retrieve
-        // more details for that place.
-        searchBox.addListener('places_changed', function() {
-          var places = searchBox.getPlaces();
+  google.maps.event.addListener(marker,'click',function() {
+  
+  map.setZoom(15);
+  map.setCenter(marker.getPosition());
+  window.setTimeout(function() {map.panTo(marker.getPosition());
+    }, 3000);
+});
 
-          if (places.length == 0) {
-            return;
-          }
+google.maps.event.addListener(spain,'click',function() {
+  
+  map.setZoom(15);
+  map.setCenter(spain.getPosition());
+  window.setTimeout(function() {map.panTo(spain.getPosition());
+    }, 3000);
+});
 
-          // Clear out the old markers.
-          markers.forEach(function(marker) {
-            marker.setMap(null);
-          });
-          markers = [];
-
-          // For each place, get the icon, name and location.
-          var bounds = new google.maps.LatLngBounds();
-          places.forEach(function(place) {
-            if (!place.geometry) {
-              console.log("Returned place contains no geometry");
-              return;
-            }
-            var icon = {
-              url: place.icon,
-              size: new google.maps.Size(71, 71),
-              origin: new google.maps.Point(0, 0),
-              anchor: new google.maps.Point(17, 34),
-              scaledSize: new google.maps.Size(25, 25)
-            };
-
-            // Create a marker for each place.
-            markers.push(new google.maps.Marker({
-              map: map,
-              icon: icon,
-              title: place.name,
-              position: place.geometry.location
-            }));
-
-            if (place.geometry.viewport) {
-              // Only geocodes have viewport.
-              bounds.union(place.geometry.viewport);
-            } else {
-              bounds.extend(place.geometry.location);
-            }
-          });
-          map.fitBounds(bounds);
-        });
-      }
+google.maps.event.addListener(france,'click',function() {
+ 
+  map.setZoom(15);
+  map.setCenter(france.getPosition());
+  window.setTimeout(function() {map.panTo(france.getPosition());
+    }, 3000);
+});
+}
